667fb62a3e1462816dff54c801a775575c5fb43e
Anjali Kantharuban <anjali@calvisitor-10-105-167-53.calvisitor.1918.berkeley.edu>
2018-09-20 16:25:39 -0700

    Homework 1 Complete

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,17 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    i = 0
+    no_duplicates = [0]
+    total = 0
+    while i < a.length
+        a[i] = a[i].to_i + 2
+        if a[i] < 10 and a[i] % 2 == 0 and not no_duplicates.index(a[i])
+            no_duplicates.push(a[i])
+            total += a[i]
+        end
+        i += 1
+    end
+    return total
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,22 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  return (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a = a.sort
+  i = 0
+  while i < a.length
+      a[i] = a[i] + 1
+      i += 1
+  end
+  return a
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
@@ -49,4 +55,9 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  t = 0
+  for n in word.chars
+      t += values[n.to_sym]
+  end
+  return t
 end
